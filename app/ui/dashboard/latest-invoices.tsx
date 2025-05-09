c2487421ea0ad98a7a18cbf5872952aac6044f9a
yosia4 <cofran583@gmail.com>
2025-05-09 17:02:20 +0700

    das

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -10,7 +10,7 @@ export default async function LatestInvoices({
   latestInvoices: LatestInvoice[];
 }) {
   return (
-    <div className="flex w-full flex-col md:col-span-6">
+    <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
@@ -55,7 +55,7 @@ export default async function LatestInvoices({
         </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
         </div>
       </div>
     </div>
