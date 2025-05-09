3f08f96284b170eae507283814e10ab29e73d029
yosia4 <cofran583@gmail.com>
2025-05-09 12:23:21 +0700

    route.ts

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-// import postgres from 'postgres';
-
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data;
-// }
-
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
-=======
 import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
@@ -46,5 +19,4 @@ export async function GET() {
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
->>>>>>> 6ee1da485abf7df8017e0952223ab801b134751a
 }
