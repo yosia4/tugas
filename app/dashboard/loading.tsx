33165c03666f66a04e586169e775fefa9ae253b5
yosia4 <cofran583@gmail.com>
2025-05-14 18:14:00 +0700

    ganti

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,6 +1,6 @@
-import DashboardSkeleton from '@/app/ui/skeletons';
+  import DashboardSkeleton from '@/app/ui/skeletons';
 
-export default function Loading() {
-    return <div className="p-4 text-center text-gray-500">Loading...</div>;
-  }
-  
+  export default function Loading() {
+      return <div className="p-4 text-center text-gray-500">Loading...</div>;
+    }
+    
